7d04379f1d1b377c4c708da308d3084caef446fd
Draburst <draburststudent@gmail.com>
2023-11-12 13:01:21 +0200

    js3.1 commit

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import MainView, HistoryViev
 
-
+#js3.1
 urlpatterns = [
     path('', MainView.as_view(), name='main'),
     path('/h/', HistoryViev.as_view(), name='history'),
